3bce97511ce7b16ce2bb3b5c7c3558fa4fabb244
Shani Elharrar <shani.elha@gmail.com>
2015-03-15 16:33:18 +0200

    Fixed a 100% cpu bug when readtimeout was -1

--- uhttpsharp/Clients/TcpClientAdapter.cs
+++ uhttpsharp/Clients/TcpClientAdapter.cs
@@ -7,15 +7,20 @@ namespace uhttpsharp.Clients
     public class TcpClientAdapter : IClient
     {
         private readonly TcpClient _client;
+        private readonly NetworkStream _stream;
 
         public TcpClientAdapter(TcpClient client)
         {
             _client = client;
+            _stream = _client.GetStream();
+
+            // Read Timeout of one second.
+            _stream.ReadTimeout = 1000;
         }
 
         public Stream Stream
         {
-            get { return _client.GetStream(); }
+            get { return _stream; }
         }
 
         public bool Connected
